16b324de2a35ae83b7456c07747b17f837f3bd43
Marta Scardillo <martascardillo02@gmail.com>
2023-11-03 16:32:28 +0100

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+</script>
+
+<template>
+  <div>
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="background">
+    
+  </div>
+</template>
+
+<style scoped>
+.background {
+    background-image: url(../assets/nahida.png);
+}
+
+</style>
